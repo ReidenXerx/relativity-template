70e66e819c0614b1ff2d487171b2acc92db427fa
ReidenXerx <vadik44444444@yandex.ua>
2020-05-21 23:40:08 +0300

    Update parallax.js

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -2,10 +2,8 @@ import $ from 'jquery';
 
 function parallax(event) {
   this.querySelectorAll('.parallax').forEach(element => {
-    var top = element.style.top + window.scrollY;
-    element.style.top = top + 'px';
-    console.log(window.scrollY);
-    console.log('top ' + element.style.top);
+    element.style.transform = `translateY(${-2 * window.scrollY}px)`;
+    console.log(element.style.transform);
   });
 }
 
