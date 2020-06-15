33153b2b3c3cd75721a49299fd75729b3e37d3a0
Pedrael <mihail_krivic@mail.ru>
2020-06-16 00:05:08 +0300

    added animationByEvent function

--- src/js/animations/handlers.js
+++ src/js/animations/handlers.js
@@ -1,31 +1,106 @@
-import animations_binds from './scss/animations/animations-binds.scss';
-console.log(animations_binds.animations_parametrs);
+//import animations_binds from './scss/animations/animations-binds.scss';
+//console.log(animations_binds.animations_parametrs);
 
-var all_classes = '';
+var all_classes = '.mihail';
 var animations_list;//animations attributes
 
-animations_binds.animations_parametrs.forEach(item => {
+//animations_binds.animations_parametrs.forEach(item => {
   //animations_list initialize
-  all_classes = all_classes + 'penis';
-});
+  //all_classes = all_classes + 'penis';
+//});
 
 var eventType = 'click'
 var all_objects = document.querySelectorAll(all_classes)
 all_objects.forEach(item => {
-  map.get(animations_list, item).get('animation-name')
+  //map.get(animations_list, item).get('animation-name')
   //поставить массив анимаций в ряд, если их несколько. Анимации будут идти в массиве
 
   // item.addEventListener(eventType, (event) => {
   //   //do something
   //   console.log(event)
   // })
-  item.onmouseover = item.onmouseout = hover
-  function hover(event) {
-    if (event.type == 'mouseover') {
-      console.log(event.currentTarget, event.target, 'in')
+
+  // item.onmouseover = item.onmouseout = hover
+  // function hover(event) {
+  //   if (event.type == 'mouseover') {
+  //     event.currentTarget.style.background = 'blue'
+  //   }
+  //   if (event.type == 'mouseout') {
+  //     console.log(event.currentTarget, event.target, 'out')
+  //     event.currentTarget.style.background = 'blue'
+  //   }
+  // }
+})
+
+function animationByEvent(className, eventType, callback) {
+  var allObjects = document.querySelectorAll(className)
+  allObjects.forEach(item => {
+
+    switch(eventType) {
+      case 'hover':
+      item.addEventListener('mouseover', (event) => {
+        callback(event, 'mouseover')
+      })
+      item.addEventListener('mouseout', (event) => {
+        callback(event, 'mouseout')
+      })
+      break;
+
+      case 'click':
+      item.addEventListener('click', (event) => {
+        callback(event)
+      })
+      break;
+
+      case 'toggle':
+      var toggler = false
+      item.addEventListener('click', (event) => {
+        if(!toggler) {
+          callback(event, 'toggled')
+          toggler = true
+        }
+        else {
+          callback(event, 'untoggled')
+          toggler = false
+        }
+      })
+      break;
+
+      case 'windowscroll':
+      window.addEventListener('scroll', (event) => {
+        callback(event, item)
+      })
+      break;
+
+      case 'itemscroll':
+      item.addEventListener('scroll', (event) => {
+        callback(event)
+      })
+      break;
+
+      case 'DOMready':
+      document.addEventListener("DOMContentLoaded", (event) => {
+        callback(event)
+      })
+      break;
+
+      default:
+        return;
     }
-    if (event.type == 'mouseout') {
-      console.log(event.currentTarget, event.target, 'out')
-    }
+
+
+  })
+}
+
+animationByEvent('.mihail', 'toggle', (e, state) => {
+  if(state == 'toggled') {
+    e.currentTarget.style.background = 'red'
   }
+  else {
+    e.currentTarget.style.background = 'blue'
+  }
+})
+animationByEvent('.mihail', 'windowscroll', (event, item) => {
+  //console.log(item)
+  item.style.color = 'red'
 })
