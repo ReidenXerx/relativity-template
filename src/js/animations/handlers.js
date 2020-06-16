38198d978c87003e98007ecf8070205b3c747448
Pedrael <mihail_krivic@mail.ru>
2020-06-16 18:39:10 +0300

    added ripple

--- src/js/animations/handlers.js
+++ src/js/animations/handlers.js
@@ -32,6 +32,40 @@ all_objects.forEach(item => {
   // }
 })
 
+var animationsList = [
+{
+  className: 'red',
+  animationType: 'hover',
+  animationName: 'blup'
+},
+{
+  className: 'red',
+  animationType: 'click',
+  animationName: 'blup'
+}
+]
+
+function Ripple(e) {
+  e.currentTarget.style.overflow = 'hidden'
+  e.currentTarget.style.position = 'relative'
+  let rect = e.currentTarget.getBoundingClientRect()
+  let x = e.clientX - rect.x
+  let y = e.clientY - rect.y
+  let ripples = document.createElement('div')
+  ripples.style.left = x + 'px'
+  ripples.style.top = y + 'px'
+  ripples.style.position = 'absolute'
+  ripples.style.background = '#fff'
+  ripples.style.transform = 'translate(-50%, -50%)'
+  ripples.style.pointerEvents = 'none'
+  ripples.style.borderRadius = '50%'
+  ripples.style.animation = 'ripple 1s linear infinite'
+  e.currentTarget.appendChild(ripples)
+  setTimeout(() => {
+    ripples.remove()
+  }, 1000)
+}
+
 function animationByEvent(className, eventType, callback) {
   var allObjects = document.querySelectorAll(className)
   allObjects.forEach(item => {
@@ -92,15 +126,18 @@ function animationByEvent(className, eventType, callback) {
   })
 }
 
-animationByEvent('.mihail', 'toggle', (e, state) => {
-  if(state == 'toggled') {
-    e.currentTarget.style.background = 'red'
-  }
-  else {
-    e.currentTarget.style.background = 'blue'
-  }
+// animationByEvent('.mihail', 'toggle', (e, state) => {
+//   if(state == 'toggled') {
+//     //e.currentTarget.style.background = 'red'
+//   }
+//   else {
+//     e.currentTarget.style.background = 'blue'
+//   }
+// })
+animationByEvent('.mihail', 'click', (e) => {
+  Ripple(e)
 })
-animationByEvent('.mihail', 'windowscroll', (event, item) => {
-  //console.log(item)
-  item.style.color = 'red'
-})
+// animationByEvent('.mihail', 'windowscroll', (event, item) => {
+//   //console.log(item)
+//   item.style.color = 'red'
+// })
