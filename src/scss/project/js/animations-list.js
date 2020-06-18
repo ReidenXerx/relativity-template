d420fefa4dd80d895967f6418984383824ec8f18
ReidenXerx <vadik44444444@yandex.ua>
2020-06-18 16:42:37 +0300

    animations list update

---
+++ src/scss/project/js/animations-list.js
@@ -0,0 +1,16 @@
+require ('./animations/timing-functions.js')
+
+var animationsList = [
+// {
+//   className: 'red',
+//   animationType: 'hover',
+//   animationName: 'blup'
+// },
+  {
+    className: '.mihail',
+    animationType: 'click',
+    animationName: 'Ripple',
+    animationTimingFunction: easeInSine,
+    animationDuration: 1s
+  }
+]
